ac6629db3d8be8b19e7454c93f345d8d2fb538d1
Anton Kovalev <tonikovalev@gmail.com>
2021-09-18 15:05:15 +0300

    Updates

---
+++ precache-manifest.31e96083b822cb97fc2a5d02f2f09fbb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "463012b497ed0c708ed8ffc2490e2383",
+    "url": "/noir/index.html"
+  },
+  {
+    "revision": "b75ad32b74b690b50830",
+    "url": "/noir/static/css/main.7fca493c.chunk.css"
+  },
+  {
+    "revision": "1bd902be4401ad94a4e3",
+    "url": "/noir/static/js/2.751ecfab.chunk.js"
+  },
+  {
+    "revision": "20a6f33d71d567d1fe0dc1c77a1a22e7",
+    "url": "/noir/static/js/2.751ecfab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3bd3b4945425a897dceb",
+    "url": "/noir/static/js/3.da948d91.chunk.js"
+  },
+  {
+    "revision": "b75ad32b74b690b50830",
+    "url": "/noir/static/js/main.72b326b6.chunk.js"
+  },
+  {
+    "revision": "df708c7d2198f698fdee",
+    "url": "/noir/static/js/runtime-main.c938bbfc.js"
+  }
+]);
